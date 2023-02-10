1de1c7d5160a3050e4ec9eaabd3356c4bac3b19a
Maxkie14 <Teerapatbuttho@gmail.com>
2023-02-10 17:02:30 +0700

    ss

--- home.php
+++ home.php
@@ -3,7 +3,9 @@
 <h1>Home page</h1>
 <div class="row mt-5">
     <div class="col-md-6">.col-sm-4</div>
-
+    <div class="col-md-6">.col-sm-4</div>
+    <div class="col-md-6">.col-sm-8</div>
+      <div class="col-md-6">.col-sm-4</div>
     <div class="col-md-6">.col-sm-4</div>
     <div class="col-md-6">.col-sm-8</div>
 </div>
