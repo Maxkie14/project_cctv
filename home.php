fd14c4f582c731a8591a0411df76240a5c083f62
Maxkie14 <Teerapatbuttho@gmail.com>
2023-02-10 16:47:00 +0700

    ssd

--- home.php
+++ home.php
@@ -3,6 +3,8 @@
 <h1>Home page</h1>
 <div class="row mt-5">
     <div class="col-md-6">.col-sm-4</div>
+
+    <div class="col-md-6">.col-sm-4</div>
     <div class="col-md-6">.col-sm-8</div>
 </div>
 @endsection
